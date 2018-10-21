b853d8a4644d79c946e8c4ad3738ab4e7c06fc66
Tim Rourke <tim@timrourke.com>
2018-10-20 21:04:40 -0500

    Make generalized test case for integration-testing controllers

---
+++ api/tests/Functional/Controller/ControllerTestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Controller;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ControllerTestCase extends WebTestCase
+{
+    /**
+     * @var \Symfony\Bundle\FrameworkBundle\Client
+     */
+    protected $client;
+
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $connection;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+
+        $this->connection = static::$container->get(Connection::class);
+
+        $this->entityManager = static::$container->get(EntityManagerInterface::class);
+
+        $this->connection->beginTransaction();
+    }
+}
