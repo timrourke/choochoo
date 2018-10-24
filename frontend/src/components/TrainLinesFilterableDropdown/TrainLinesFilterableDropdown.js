4cfa6aa701820333d9036eed9a6623ab5c194b9e
Tim Rourke <tim@timrourke.com>
2018-10-23 20:52:17 -0500

    Install DoctrineExtensions to support LPAD MySQL function

---
+++ frontend/src/components/TrainLinesFilterableDropdown/TrainLinesFilterableDropdown.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import FilterableDropdown from './../FilterableDropdown';
+
+const getButtonLabel = (selectedOperator) => {
+  if (!selectedOperator) {
+    return 'Nothing selected.';
+  }
+
+  return `${selectedOperator.firstName} ${selectedOperator.lastName}`;
+};
+
+const getOptionsFromOperators = (operators) => {
+  return operators.map((operator) => {
+    return {
+      ...operator,
+      label: `${operator.firstName} ${operator.lastName}`,
+    };
+  });
+};
+
+export default class OperatorsFilterableDropdown extends Component {
+  componentDidMount() {
+    this.props.queryOperators();
+  };
+
+  render() {
+    return (
+      <FilterableDropdown
+        buttonLabel={getButtonLabel(this.props.operators.selectedOperator)}
+        filterOptions={this.props.queryOperators}
+        selectOption={this.props.selectOperator}
+        options={getOptionsFromOperators(this.props.operators.operators)}
+        selectedOption={this.props.operators.selectedOperator}
+      />
+    );
+  }
+}
